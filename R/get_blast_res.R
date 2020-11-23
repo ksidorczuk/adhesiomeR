8fbc7055c7f51fe2a5ba222910989f7a3244b427
ksidorczuk <sidorczuk.katarzyna17@gmail.com>
2020-11-23 17:04:51 +0100

    finished first functions for package

--- R/get_blast_res.R
+++ R/get_blast_res.R
@@ -1,11 +1,60 @@
-get_blast_res <- function(input_file_list) {
+#' Get BLAST results for many files
+#' 
+#' This function runs BLAST search on a provided list of files.
+#' @param input_file_list A \code{list} of file names that will be used
+#' as input files for BLAST search. File have to contain nucleotide sequences
+#' in a FASTA format. 
+#' @param blast_dir A path to the directory with BLAST executables. By default,
+#' it tries to find the proper executable by running system command \code{which}
+#' (except on Windows). If the \code{blastn} executable is not found by default,
+#' you should provide a path to the directory in which it is located.
+#' @return A data frame with BLAST results containing following information: 
+#' query name, subject name, % of identity, alignment length, mismatches,
+#' gap opens, query start, query end, subject start, subject end, E-value,
+#' bit score (for detailed information about those outputs, please see BLAST
+#' documentation), as well as name of a file that was used as an input.
+#' @seealso do_blast_single
+#' @export
+get_blast_res <- function(input_file_list, blast_dir = Sys.which("blastn")) {
   lapply(input_file_list, function(ith_file) {
-    do_blast_single(ith_file)
+    do_blast_single(ith_file, blast_dir)
   }) %>% bind_rows()
 }
 
-do_blast_single <- function(input_file) {
-  system(paste0("blastn -db db/adhesins -query ", input_file, " -out ", input_file, ".blast -outfmt 6"))
+
+#' Get BLAST results for a single file
+#' 
+#' This function runs BLAST search on a provided file.
+#' @param input_file Name of a file that will be used as input files for BLAST search. 
+#' File have to contain nucleotide sequences in a FASTA format. 
+#' @param blast_dir A path to the directory with BLAST executables. By default,
+#' it tries to find the proper executable by running system command \code{which}
+#' (except on Windows). If the \code{blastn} executable is not found by default,
+#' you should provide a path to the directory in which it is located.
+#' @return A data frame with BLAST results containing following information: 
+#' query name, subject name, % of identity, alignment length, mismatches,
+#' gap opens, query start, query end, subject start, subject end, E-value,
+#' bit score (for detailed information about those outputs, please see BLAST
+#' documentation), as well as name of a file that was used as an input.
+#' @seealso get_blast_res
+#' @export
+do_blast_single <- function(input_file, blast_dir = Sys.which("blastn")) {
+  validate_input_file(input_file)
+  if(blast_dir == "") {
+    stop("It seems that you do not have BLAST installed. To be able to use adhesiomeR,
+    you should install standalone BLAST. If you have installed BLAST and still
+    see this message, please use 'blast_dir' argument to provide a proper path to 
+    the BLAST directory in which 'blastn' executable is located.")
+  } else if(blast_dir == Sys.which("blastn")) {
+    system(paste0(blast_dir, " -db db/adhesins -query ", input_file, " -out ", input_file, ".blast -outfmt 6"))
+  } else {
+    if(grepl("/$", blast_dir)) blast_dir <- gsub("/$", "", blast_dir)
+    tryCatch(system(paste0(blast_dir, "/blastn -db db/adhesins -query ", input_file, " -out ", input_file, ".blast -outfmt 6")),
+             warning = function(w) {
+               msg <- conditionMessage(w)
+               if(msg == "error in running command") message("Please check if the BLAST directory path you provided is correct.")
+             })
+  }
   name <- last(strsplit(input_file, "/")[[1]])
   res <- read.delim(paste0(input_file, ".blast"), header = FALSE)
   colnames(res) <- c("Query", "Subject", "% identity", "Alignment length", "Mismatches",
@@ -13,3 +62,4 @@ do_blast_single <- function(input_file) {
   file.remove(paste0(input_file, ".blast"))
   mutate(res, File = name)
 }
+
--- R/get_genes_systems_df.R
+++ R/get_genes_systems_df.R
@@ -1,20 +1,41 @@
+#' Get gene-system data frame
+#' 
+#' Extract systems and corresponding genes from a sequence
+#' file used for database creation.
+#' @param seq_file File with sequences used for database creation 
+#' in a FASTA format. See details section for FASTA header format requirements.
+#' @return a data frame containing systems and corresponding genes.
+#' It consists of three columns:
+#' \describe{
+#'   \item{Gene}{Gene name}
+#'   \item{System}{System to which given gene is assigned}}
+#' Each row contains only one gene and system, so in case of genes assigned to 
+#' multiple systems, they will occupy several rows.
+#' @details The sequence file should be in a FASTA format with headers 
+#' containing database name, gene name, accession, system name and each element
+#' should be separated by \code{~} without spaces, e.g. 
+#' \code{>Adhesins~fimH~NC_000913.3:4548808-4549710~Type_1_Fimbriae}. If a gene
+#' is assigned to multiple systems, system names should be separated using 
+#' semicolons without spaces, e.g. \code{Adhesins~aggR~NC_019000.1:48472-49269~AA/I_Fimbriae;
+#' AA/II_Fimbriae;AA/III_Fimbriae}.
+#' 
+#' @export
+#' @examples 
+#' get_genes_in_systems_db("data/Adhesins_sequences")
+#' 
+#' @importFrom dplyr mutate
 get_genes_in_systems_db <- function(seq_file) {
   data_file <- readLines(seq_file)
   def_lines <- data_file[grepl(">", data_file)]
+  unique_genes <- unique(sapply(def_lines, function(x) strsplit(x, "~")[[1]][2], USE.NAMES = FALSE)) 
   df <- data.frame(Gene = sapply(def_lines, function(x) strsplit(x, "~")[[1]][2], USE.NAMES = FALSE),
                    System = sapply(def_lines, function(x) strsplit(x, "~")[[1]][4], USE.NAMES = FALSE),
                    stringsAsFactors = FALSE) %>% 
     mutate(System = gsub("^ ", "", System))
-  # unique_systems <- unique(df[["systems"]]) %>% 
-  #   sapply(., function(x) strsplit(x, " ")[[1]], USE.NAMES = FALSE) %>% 
-  #   unlist() %>% 
-  #   unique()
-  # lapply(unique_systems, function(ith_system) {
-  #   genes <- filter(df, systems == ith_system)[["gene"]] %>% 
-  #     paste0(collapse = ", ")
-  #   data.frame(System = ith_system,
-  #              Genes = genes,
-  #              stringsAsFactors = FALSE)
-  # }) %>% bind_rows()
-  df
+  lapply(unique_genes, function(ith_gene) {
+    data.frame(Gene = ith_gene,
+               System = strsplit(df[["System"]][which(df[["Gene"]] == ith_gene)], ";")[[1]],
+               stringsAsFactors = FALSE) %>% 
+      mutate(System = gsub("_", " ", System))
+  }) %>% bind_rows()
 }
