4d354f424866cb0d60a36f1991a12b36bd1eeaba
ksidorczuk <sidorczuk.katarzyna17@gmail.com>
2021-01-05 20:50:52 +0100

    updated fasta format check

--- R/utils.R
+++ R/utils.R
@@ -2,6 +2,7 @@
 validate_input_file <- function(input_file) {
   if(!file.exists(input_file)) stop(paste0("File ", input_file, "does not exist.")) 
   x <- readLines(input_file)
+  if(!(grepl("^>", x[1]))) stop("There is no '>' at the beginning of the file. Input file has to be in a FASTA format.")
   if(length(x) < 1) stop("Input file is empty.")
   if(length(x) < 2) stop("Input file has to be in a FASTA format.")
   if(sum(grepl("^>", x)) == 0) stop("Input file has to be in a FASTA format.")
