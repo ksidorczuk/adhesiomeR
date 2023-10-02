a886036536f65c00bb438376213aa407f14e69f2
ksidorczuk <sidorczuk.katarzyna17@gmail.com>
2023-10-02 18:43:19 +0100

    fixed gc_to_sample

--- R/metagenomes.R
+++ R/metagenomes.R
@@ -5,7 +5,11 @@
 #' @param blast_res a data frame with BLAST search results obtained by using
 #' \code{\link{get_blast_res}}. 
 #' @param presence_absence_file A path to the abundance matrix file in
-#' a tab-delimited format. The first column is expected to contain gene names.
+#' a tab-delimited format. The first column is expected to be named 'Gene' 
+#' and contain gene names
+#' @param threshold abundance threshold to consider a gene as present in a sample.
+#' The default value is \code{NULL} and means that samples with any abundance
+#' larger than 0 will be used. 
 #' @return A data frame with BLAST results expanded to individual samples
 #' @seealso get_blast_res
 #' @importFrom dplyr bind_rows left_join select 
@@ -13,7 +17,6 @@
 #' @export
 gc_to_sample <- function(blast_res, abundance_matrix, threshold = NULL) {
   ab_mat <- read.delim(abundance_matrix)
-  first_col <- colnames(ab_mat)[1]
   tresh <- if(is.null(threshold)) {
     0 
   } else {
@@ -21,17 +24,15 @@ gc_to_sample <- function(blast_res, abundance_matrix, threshold = NULL) {
   }
   long_df <- bind_rows(
     lapply(1:nrow(ab_mat), function(i) {
-      print(i)
       sel <- which(ab_mat[i, 2:ncol(ab_mat)] > threshold)
       if(length(sel) != 0) {
         data.frame(File = colnames(ab_mat)[sel+1],
-                   Gene = ab_mat[[first_col]][i])
+                   Gene = ab_mat[["Gene"]][i])
       } else {
         data.frame(File = c(),
                    Gene = c())
       }
     })
   )
-  
-  left_join(select(blast_res, -File), long_df, by = c("Query" = first_col), relationship = "many-to-many")
+  left_join(select(blast_res, -File), long_df, by = c("Query" = "Gene"), relationship = "many-to-many")
 }
